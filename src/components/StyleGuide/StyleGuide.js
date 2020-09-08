9d6130c4fd027dd877d5720312cd90d5093797bf
Joey Leger <joseph.leger@duke-energy.com>
2020-09-08 10:01:29 -0400

    added some scraped image assets

--- src/components/StyleGuide/StyleGuide.js
+++ src/components/StyleGuide/StyleGuide.js
@@ -9,11 +9,20 @@ const StyleGuide = () => {
     return Array.from(Array(5), (_, i) => <Swatch scale={i + 1} />);
   };
 
-  const FlexWrapper = ({ children }) => <div style={{ display: 'flex' }}>{children}</div>;
+  const FlexWrapper = ({ children }) => (
+    <div style={{ alignItems: 'flex-start', display: 'flex' }}>{children}</div>
+  );
 
   const Heading = ({ children }) => (
     <h2 style={{ color: '#7b8ba4', marginTop: '100px' }}>{children}</h2>
   );
+
+  const Images = () => {
+    const fileNames = ['background', 'splash-enter', 'splash-logo', 'splash-logoextend'];
+    return fileNames.map((elm, index) => (
+      <img src={require(`../../assets/images/${elm}.gif`)} key={index} alt={`${elm} example`} />
+    ));
+  };
 
   const Section = ({ children, title }) => {
     return (
@@ -37,6 +46,11 @@ const StyleGuide = () => {
       </Section>
       <Section title='Typography'>
         <Typography />
+      </Section>
+      <Section title='Image Reference'>
+        <FlexWrapper>
+          <Images />
+        </FlexWrapper>
       </Section>
       <Section title='Identity'>
         <Logos />
